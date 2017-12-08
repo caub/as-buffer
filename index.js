8912e303e26cf2dff67337262a8e98027388055e
caub <cyril.auburtin@gmail.com>
2017-12-08 17:52:33 +0100

    node < 4

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
-module.exports = (s, _maxsize) =>
-	new Promise((resolve, reject) => {
+if (!typeof Promise !== 'function') {
+	global.Promise = require('es6-promise');
+}
+
+module.exports = function(s, _maxsize) {
+	return new Promise(function (resolve, reject) {
 		if (Buffer.isBuffer(s) || Array.isArray(s) || typeof s === 'string') {
 			return resolve(Buffer.from(s));
 		}
@@ -7,11 +11,11 @@ module.exports = (s, _maxsize) =>
 		var bufs = [];
 		var maxsize = _maxsize || 1e7;
 		var size = 0;
-		s.on('data', d => {
+		s.on('data', function (d) {
 			bufs.push(d);
 			size += d.length;
 			if (size > maxsize) {
-				var err = new Error(`File too large, max: ${Math.round(maxsize/1000)}kB`);
+				var err = new Error('File too large, max:' + Math.round(maxsize/1000) + 'kB');
 				err.status = 413;
 				reject(err);
 			}
@@ -28,3 +32,4 @@ module.exports = (s, _maxsize) =>
 			resolve(s._readableState && s._readableState.objectMode ? bufs : Buffer.concat(bufs));
 		}
 	});
+};
